689e0353aa1202a663a7e0a7fe8faf3e8069d65f
github username <github email address>
2018-11-21 19:31:33 -0500

    index

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+console.log(hogs)
 
 class App extends Component {
   render() {
