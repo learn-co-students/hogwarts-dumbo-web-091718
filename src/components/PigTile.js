5578002d1cba62465e45faddd1a90d62be8fd575
shemarjoseph <shemarjoseph@yahoo.com>
2018-11-26 12:44:10 -0500

    anotha 1

--- src/components/PigTile.js
+++ src/components/PigTile.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
 const PigTile = (props) => {
-  const toggleInfo = () => {
-    const template =  <div>
-                        <div>Specialty: {props.specialty}</div>
-                        <div>Weight: {props.weight}</div>
-                        <div>Highest Medal Achieved: {props.highestMedalAchieved}</div>
-                      </div>
-    return props.wasClickedOn && template 
-  }
+  const template = <div>
+    <div>Specialty: {props.specialty}</div>
+    <div>Weight:    {props.weight}</div>
+    <div>Highest Medal Achieved: {props.highestMedalAchieved}</div>
+  </div>
 
   return (
     <div className="pigTile" onClick={props.handleClick}>
       <img src={require(`../hog-imgs/${props.pic}`)} alt="piggy" />
       <div>Name: {props.name}</div>
-      { toggleInfo() }
+      { props.wasClickedOn && template }
     </div>
   )
 }
