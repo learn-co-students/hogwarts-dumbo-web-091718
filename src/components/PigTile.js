e769a294b5f7a50f30b586029a0c699c0cb6b931
shemarjoseph <shemarjoseph@yahoo.com>
2018-11-26 12:38:49 -0500

    refactor PigTile

--- src/components/PigTile.js
+++ src/components/PigTile.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
 const PigTile = (props) => {
+  const toggleInfo = () => {
+    const template =  <div>
+                        <div>Specialty: {props.specialty}</div>
+                        <div>Weight: {props.weight}</div>
+                        <div>Highest Medal Achieved: {props.highestMedalAchieved}</div>
+                      </div>
+    return props.wasClickedOn && template 
+  }
+
   return (
     <div className="pigTile" onClick={props.handleClick}>
-    <img
-      src={require(`../hog-imgs/${props.pic}`)}
-      alt="piggy"
-    />
+      <img src={require(`../hog-imgs/${props.pic}`)} alt="piggy" />
       <div>Name: {props.name}</div>
-      {
-        props.wasClickedOn && (
-          <div>
-            <div>Specialty: {props.specialty}</div>
-            <div>Weight: {props.weight}</div>
-            <div>Highest Medal Achieved: {props.highestMedalAchieved}</div>
-          </div>
-        )
-      }
+      { toggleInfo() }
     </div>
   )
 }
